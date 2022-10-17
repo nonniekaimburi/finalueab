90ed857896bd8b788483a8a47d001d7a4650cea6
nonniekaimburi <nonniekaimburi@gmail.com>
2022-10-17 17:57:17 +0300

    mmimi

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,13 +5,14 @@ export default defineNuxtConfig({
     },
     runtimeConfig: {
       // The private keys which are only available within server-side
-      API_KEY: process.env.API_KEY,
-      AUTH_DOMAIN:process.env.AUTH_DOMAIN,
-      PROJECT_ID:process.env.PROJECT_ID,
-      STORAGE_BUCKET:process.env.STORAGE_BUCKET,
-      MESSAGING_SENDER_ID:process.env.MESSAGING_SENDER_ID,
-      APP_ID:process.env.APP_ID,
-      MEASUREMENT_ID: process.env.MEASUREMENT_ID,
+      apiKey: "AIzaSyDjJNBybOF7g_zeslOezDYSg5czTlnbAS0",
+      authDomain: "ueabfms.firebaseapp.com",
+      databaseURL: "https://ueabfms-default-rtdb.firebaseio.com",
+      projectId: "ueabfms",
+      storageBucket: "ueabfms.appspot.com",
+      messagingSenderId: "361434967495",
+      appId: "1:361434967495:web:f6423a04482ca828be3de2",
+      measurementId: "G-ZJMEHG1NKE"
       // Keys within public, will be also exposed to the client-side
       public: {
         API_KEY: process.env.API_KEY,
